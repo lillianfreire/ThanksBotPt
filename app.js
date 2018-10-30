dcec3c0d54e145e16ffc8a851b4a52d9bf51473b
Lillian F <32270233+lillianfreire@users.noreply.github.com>
2018-10-30 11:49:11 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,10 @@
  *
  */
 
+const server = app.listen(8080, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+
 const
     crypto = require('crypto'),
     express = require('express'),
